d3fa64b5180ed511a75dba5233038cd1b1efedae
Konrad <konradaccount@emailn.de>
2023-06-05 15:18:13 +0200

    versionfour

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('config.js');
-console.log('versiondrei');
+console.log('versionfour');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('index.js');
-console.log('versiondrei');
+console.log('versionfour');
